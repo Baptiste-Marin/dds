5b980cc59a191f09e2cfe4153dc445cd7cf4c669
Baptiste-Marin <baptistemv@gmail.com>
2024-09-16 17:33:10 +0200

    initial commit

---
+++ tests/main_test.py
@@ -0,0 +1,10 @@
+import unittest
+from baptiste.main import donne_des_sous
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        compte_en_banque = donne_des_sous(8, 10)
+        self.assertEqual(18, compte_en_banque)
+
+if __name__ == '__main__':
+    unittest.main()
